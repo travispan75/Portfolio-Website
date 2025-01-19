269aa21fd195345187eebb3286357f15667592c5
Travis Pan <trakabeth@gmail.com>
2025-01-18 20:50:09 -0500

    fixed mobile responsiveness for contact info

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -10,20 +10,22 @@ const Contact = () => {
                     <h2 className="text-white text-center text-3xl font-bold">CONTACT INFO</h2>
                 </div>
             </FadeUp>
-            <div className="text-white flex gap-24 justify-center items-center">
-                <div className="flex items-center space-x-2">
-                    <PiEnvelope />
-                    <a href="mailto:travispan75@gmail.com" className="text-white">
-                        <h3>travispan75@gmail.com</h3>
-                    </a>
+            <FadeUp>
+                <div className="text-white flex flex-wrap gap-8 sm:gap-24 justify-center items-center px-8">
+                    <div className="flex items-center space-x-2">
+                        <PiEnvelope />
+                        <a href="mailto:travispan75@gmail.com" className="text-white">
+                            <h3>travispan75@gmail.com</h3>
+                        </a>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <FiPhone />
+                        <a href="tel:+16472978886" className="text-white">
+                            <h3>647-297-8886</h3>
+                        </a>
+                    </div>
                 </div>
-                <div className="flex items-center space-x-2">
-                    <FiPhone />
-                    <a href="tel:+16472978886" className="text-white">
-                        <h3>647-297-8886</h3>
-                    </a>
-                </div>
-            </div>
+            </FadeUp>
         </div>
     );
 }
